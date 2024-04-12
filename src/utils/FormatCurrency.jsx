b9e9a3f395e1975dede4539f1885c0ffc033a69a
pudhi99 <prudhvi.inumarthi99@gmail.com>
2024-04-12 20:41:49 +0530

    second commit

--- src/utils/FormatCurrency.jsx
+++ src/utils/FormatCurrency.jsx
@@ -1,20 +1,21 @@
+import React from "react";
 
-import React, { Children } from 'react'
-
-function FormatCurrency({children}) {
+function FormatCurrency({ children }) {
   const formatCurrency = (amount) => {
-    const money =  new Intl.NumberFormat('en-IN', {
-      style: 'currency',
-      currency: 'INR',
+    const money = new Intl.NumberFormat("en-IN", {
+      style: "currency",
+      currency: "INR",
       minimumFractionDigits: 0,
       maximumFractionDigits: 0,
     }).format(amount);
-    return money
+    return money;
   };
-  
+
   return (
-    <div><p className='heading'>{formatCurrency(children)}</p></div>
-  )
+    <div>
+      <p className="heading">{formatCurrency(children)}</p>
+    </div>
+  );
 }
 
-export default FormatCurrency
+export default FormatCurrency;
