d2dab8c65ee86b9b97a58e029d28e1dc3d23b27b
pudhi99 <prudhvi.inumarthi99@gmail.com>
2024-04-13 12:10:52 +0530

    adding google add sense

--- src/app/layout.js
+++ src/app/layout.js
@@ -34,6 +34,13 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4777990936225991"
+          crossorigin="anonymous"
+        ></script>
+      </head>
       <body className="">
         <div className="h-screen overflow-y-auto">
           {children}
